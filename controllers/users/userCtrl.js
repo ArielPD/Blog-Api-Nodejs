aa37c26732e8454a425d3fbdf8c3d59ad442bb81
Ariel Pinto <arielpinto1041@gmail.com>
2023-03-04 23:17:15 -0400

    Generate token JWT

--- controllers/users/userCtrl.js
+++ controllers/users/userCtrl.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs'); 
 const User = require ("../../model/User/User");
+const generateToken = require("../../utils/generateToken");
 
 //Register
 const userRegisterCtrl = async(req, res)=> {
@@ -56,7 +57,13 @@ const userLoginCtrl = async(req, res)=> {
 
         res.json({
            status: 'success',
-           data: userFound 
+           data: {
+                firstname: userFound.firstname,
+                lastname: userFound.lastname,
+                email: userFound.email,
+                isAdmin: userFound.isAdmin,
+                token: generateToken(userFound._id),
+           }, 
         })
     } catch(error) {
         res.json(error.message)
